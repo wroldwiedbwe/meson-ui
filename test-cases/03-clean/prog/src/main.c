097ca48c0a7ef074ce481efe78a91e7535d8cc5c
Michael Brockus <squidfarts@icloud.com>
2019-10-14 09:33:45 -0700

    [ADDING] Adding cases for Meson wrapper class.

---
+++ test-cases/03-clean/prog/src/main.c
@@ -0,0 +1,18 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.c                                                                    *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michaelbrockus@icloud.com>                                     *
+ *                                                                                 *
+ ***********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    puts("Hello, c.");
+    
+    return EXIT_SUCCESS;
+}// end of function main
