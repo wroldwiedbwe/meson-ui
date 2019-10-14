adeeacd9b1ea0938d5053926e155f18042f81ca4
Michael Brockus <squidfarts@icloud.com>
2019-10-14 11:24:33 -0700

    [ADDING] Docker image parts.

---
+++ ciimage/run_install.py
@@ -0,0 +1,43 @@
+#!/user/bin/env python3
+###################################################################################
+#                                                                                 #
+# NAME: run_install.py                                                            #
+#                                                                                 #
+# AUTHOR: Michael Brockus.                                                        #
+#                                                                                 #
+# CONTACT: <mailto:michaelbrockus@icloud.com>                                     #
+#                                                                                 #
+# NOTICES:                                                                        #
+#                                                                                 #
+# License: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0                 #
+#                                                                                 #
+###################################################################################
+import subprocess
+
+apt_get_deps = [
+    'git', 
+    'gcc',
+    'unzip',
+    'libcurl3',
+    'python3',
+    'pkg-config', 
+    'python3-pip', 
+    'ninja-build',
+    'build-essential']
+    
+pip_get_deps = [
+    'meson',
+    'PyQt5', 
+    'codecov', 
+    'pytest', 
+    'pytest-qt', 
+    'pyinstaller']
+
+if __name__ == "__main__":
+    for dep in apt_get_deps:
+        subprocess.check_call(['apt-get', '-y', 'install', dep])
+
+    subprocess.check_call(['pip3', 'install', '--upgrade', 'pip'])
+
+    for dep in pip_get_deps:
+        subprocess.check_call(['pip3', 'install', dep])
---
+++ ciimage/run_startup.py
@@ -0,0 +1,30 @@
+#!/user/bin/env python3
+###################################################################################
+#                                                                                 #
+# NAME: run_startup.py                                                            #
+#                                                                                 #
+# AUTHOR: Michael Brockus.                                                        #
+#                                                                                 #
+# CONTACT: <mailto:michaelbrockus@icloud.com>                                     #
+#                                                                                 #
+# NOTICES:                                                                        #
+#                                                                                 #
+# License: Apache 2.0 :http://www.apache.org/licenses/LICENSE-2.0                 #
+#                                                                                 #
+###################################################################################
+
+
+def start_up_message():
+    print('***************************************')
+    print('*                                     *')
+    print('*      Your CI/CD Image is set.       *')
+    print('*  ================================   *')
+    print('*                                     *')
+    print('***************************************')
+    print('***************************************')
+    print('* Lets do some work.               :) *')
+    print('***************************************')
+    print('---------------------------------------')
+
+if __name__ == "__main__":
+    start_up_message()
