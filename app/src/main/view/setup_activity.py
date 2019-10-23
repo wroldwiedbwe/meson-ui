51c657dcd76aee71c78390606794af469ae43d49
Michael Brockus <squidfarts@icloud.com>
2019-10-23 10:01:10 -0700

    Added more options in setup activity

--- app/src/main/view/setup_activity.py
+++ app/src/main/view/setup_activity.py
@@ -24,7 +24,7 @@ class SetupActivity(QMainWindow, Ui_SetupDialog):
         super(Ui_SetupDialog, self).__init__()
         self.setupUi(self)
         
-        self.setFixedSize(715, 364)
+        self.setFixedSize(730, 464)
         self._sub_controller = controller
         self._sub_model = model
         self._parent = parent
@@ -57,22 +57,48 @@ class SetupActivity(QMainWindow, Ui_SetupDialog):
         return 'true' if self.combo_box_werror.currentText() == 'true' else 'false'
     # end of method
 
+    def get_debug(self) -> str:
+        return 'true' if self.combo_box_debug.currentText() == 'true' else 'false'
+    # end of method
+
+    def get_strip(self) -> str:
+        return 'true' if self.combo_box_strip.currentText() == 'true' else 'false'
+    # end of method
+
     def onclick_setup(self):
         self.close()
         if self.curr_build_dir.text() != '':
             meson_args = [
+                f'--auto-features={self.combo_box_auto_feature.currentText()}',
                 f'--backend={self.get_backend()}',
                 f'--buildtype={self.combo_box_buildtype.currentText()}',
-                f'--warnlevel={self.combo_box_warning.currentText()}',
+                f'--default-library={self.combo_box_default_library.currentText()}',
                 f'--layout={self.combo_box_layout.currentText()}',
-                f'--optimization={self.combo_box_optimization.currentText()}']
-                #
-                # Need to add more options from Meson built in core group 
-                #
+                f'--optimization={self.combo_box_optimization.currentText()}',
+                f'--unity={self.combo_box_unity.currentText()}',
+                f'--warnlevel={self.combo_box_warning.currentText()}',
+                f'--wrap-mode={self.combo_box_wrap_mode.currentText()}',
+                f'-Db_asneeded={self.combo_box_b_asneeded.currentText()}',
+                f'-Db_colorout={self.combo_box_b_colorout.currentText()}',
+                f'-Db_coverage={self.combo_box_b_coverage.currentText()}',
+                f'-Db_lto={self.combo_box_b_lto.currentText()}',
+                f'-Db_lundef={self.combo_box_b_lundef.currentText()}',
+                f'-Db_ndebug={self.combo_box_b_ndebug.currentText()}',
+                f'-Db_pch={self.combo_box_b_pch.currentText()}',
+                f'-Db_pgo={self.combo_box_b_pgo.currentText()}',
+                # f'--b_pie={self.combo_box_b_pic.currentText()}',  Some issues with this flag
+                f'-Db_sanitize={self.combo_box_b_sanitize.currentText()}',
+                f'-Db_staticpic={self.combo_box_b_staticpic.currentText()}']
 
             if self.get_werror() == 'true':
                 meson_args.extend(['--werror'])
             
+            if self.get_debug() == 'true':
+                meson_args.extend(['--debug'])
+
+            if self.get_strip() == 'true':
+                meson_args.extend(['--strip'])
+
             self._parent.meson.setup(meson_args)
     # end of method
 
